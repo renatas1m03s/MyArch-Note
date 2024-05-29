d4335e2b94918d857d8280cffb323c8c66cfb62e
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2024-05-29 14:09:47 -0300

    Delete 08_apps.sh

--- 08_apps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yay -Sy --noconfirm timeshift microsoft-edge-stable-bin teams steam-native-runtime protonup-qt
-
-sudo pacman -Sy --noconfirm avidemux-qt gimp kdenlive remmina obs-studio darktable wine lutris telegram-desktop onlyoffice-bin
